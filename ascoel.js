853777bffa202d66bb1f7eb578b87b72afbb4d11
Király Tamás <Kirly Tams>
2017-02-05 02:12:12 +0100

    added ir868lr alive message

--- ascoel.js
+++ ascoel.js
@@ -1,18 +1,45 @@
 var Parser = require('binary-parser').Parser;
 
 module.exports = {
-    CO868LR: function (payload) {
-        if (payload) {
-            var decoded = new Parser()
-                .endianess('big')
-                .bit1('BatteryType')
-                .bit7('BatteryLevel')
-                .floatle('TValue')
-                .floatle('RHValue')
-                .uint16('CO2Value');
-            var buf = new Buffer(payload, 'hex');
-            return decoded.parse(buf);
+    CO868LR: function (payload, port = 9) {
+        switch (port) {
+            case 9:
+                if (payload) {
+                    var decoded = new Parser()
+                        .endianess('big')
+                        .bit1('BatteryType')
+                        .bit7('BatteryLevel')
+                        .floatle('TValue')
+                        .floatle('RHValue')
+                        .uint16('CO2Value');
+                    var buf = new Buffer(payload, 'hex');
+                    return decoded.parse(buf);
+                }
+                return null;
+            default:
+                throw new Error("unknown LoRa port number: " + port);
         }
-        else return null;
+    },
+    IR868LR: function (payload, port = 9) {
+        switch (port) {
+            case 9:
+                if (payload) {
+                    var decoded = new Parser()
+                        .endianess('big')
+                        .bit1('BatteryType')
+                        .bit7('BatteryLevel')
+                        .bit5('Reserved')
+                        .bit1('BatteryAlarm')
+                        .bit1('TamperAlarm')
+                        .bit1('IntrusionAlarm')
+                        .uint8('OpCnt');
+                    var buf = new Buffer(payload, 'hex');
+                    return decoded.parse(buf);
+                }
+                return null;
+            default:
+                throw new Error("unknown LoRa port number: " + port);
+        }
+
     }
 }
--- test/ascoel.js
+++ test/ascoel.js
@@ -1,28 +1,53 @@
 var assert = require('assert');
 var converter = require("./../ascoel")
-var payload = 'e4a432704160684e4202b5'
 
-describe('Ascoel', function() {
-  describe('CO868LR', function() {
-
-    it(`should return 100 BatteryLevel with payload ${payload}`, function() {
-      assert.equal(100, converter.CO868LR(payload).BatteryLevel);
+describe('Ascoel', function () {
+  describe('CO868LR', function () {
+    var payloadCO868LR = 'e4a432704160684e4202b5'
+    it(`should return 100 BatteryLevel with payload ${payloadCO868LR}`, function () {
+      assert.equal(100, converter.CO868LR(payloadCO868LR).BatteryLevel);
     });
 
-    it(`should return 1 BatteryType with payload ${payload}`, function() {
-      assert.equal(1, converter.CO868LR(payload).BatteryType);
+    it(`should return 1 BatteryType with payload ${payloadCO868LR}`, function () {
+      assert.equal(1, converter.CO868LR(payloadCO868LR).BatteryType);
     });
 
-    it(`should return 15.01236343383789 TValue with payload ${payload}`, function() {
-      assert.equal(15.01236343383789, converter.CO868LR(payload).TValue);
+    it(`should return 15.01236343383789 TValue with payload ${payloadCO868LR}`, function () {
+      assert.equal(15.01236343383789, converter.CO868LR(payloadCO868LR).TValue);
     });
 
-    it(`should return 51.6019287109375 RHValue with payload ${payload}`, function() {
-      assert.equal(51.6019287109375, converter.CO868LR(payload).RHValue);
+    it(`should return 51.6019287109375 RHValue with payload ${payloadCO868LR}`, function () {
+      assert.equal(51.6019287109375, converter.CO868LR(payloadCO868LR).RHValue);
     });
 
-    it(`should return 693 CO2Value with payload ${payload}`, function() {
-      assert.equal(693, converter.CO868LR(payload).CO2Value);
+    it(`should return 693 CO2Value with payload ${payloadCO868LR}`, function () {
+      assert.equal(693, converter.CO868LR(payloadCO868LR).CO2Value);
+    });
+  });
+
+
+  describe('IR868LR', function () {
+    var payloadIR868LR = 'e40008ac'
+    it(`should return 100 BatteryLevel with payload ${payloadIR868LR}`, function () {
+      assert.equal(100, converter.IR868LR(payloadIR868LR).BatteryLevel);
+    });
+
+    it(`should return 1 BatteryType with payload ${payloadIR868LR}`, function () {
+      assert.equal(1, converter.IR868LR(payloadIR868LR).BatteryType);
+    });
+
+    it(`should return 0 BatteryAlarm with payload ${payloadIR868LR}`, function () {
+      assert.equal(0, converter.IR868LR(payloadIR868LR).BatteryAlarm);
+    });
+
+    it(`should return 0 IntrusionAlarm with payload ${payloadIR868LR}`, function () {
+      assert.equal(0, converter.IR868LR(payloadIR868LR).IntrusionAlarm);
+    });
+
+    it(`should return 8 OpCnt with payload ${payloadIR868LR}`, function () {
+      assert.equal(8, converter.IR868LR(payloadIR868LR).OpCnt);
     });
   });
 });
+
+
