c93916e435b4da24e56ecba087e22c0c1a510fd6
Király Tamás <Kirly Tams>
2017-02-05 20:06:33 +0100

    build(releasing): added travis config, conventional commit, and semantic-release

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # lora-app-payloader
 
 [![Build Status](https://travis-ci.org/tkiraly/lora-app-payloader.svg?branch=master)](https://travis-ci.org/tkiraly/lora-app-payloader)
+[![Dep Status](https://david-dm.org/tkiraly/lora-app-payloader.svg)](https://david-dm.org/tkiraly/lora-app-payloader)
--- test/ascoel.js
+++ test/ascoel.js
@@ -1,27 +1,27 @@
-var assert = require('assert');
+var expect = require('chai').expect;
 var converter = require("./../ascoel")
 
 describe('Ascoel', function () {
   describe('CO868LR', function () {
     var payloadCO868LR = 'e4a432704160684e4202b5'
     it(`should return 100 BatteryLevel with payload ${payloadCO868LR}`, function () {
-      assert.equal(100, converter.CO868LR(payloadCO868LR).BatteryLevel);
+      expect(converter.CO868LR(payloadCO868LR).BatteryLevel).to.eq(100);
     });
 
     it(`should return 1 BatteryType with payload ${payloadCO868LR}`, function () {
-      assert.equal(1, converter.CO868LR(payloadCO868LR).BatteryType);
+      expect(converter.CO868LR(payloadCO868LR).BatteryLevel).to.eq(100);
     });
 
     it(`should return 15.01236343383789 TValue with payload ${payloadCO868LR}`, function () {
-      assert.equal(15.01236343383789, converter.CO868LR(payloadCO868LR).TValue);
+      expect(converter.CO868LR(payloadCO868LR).TValue).to.eq(15.01236343383789);
     });
 
     it(`should return 51.6019287109375 RHValue with payload ${payloadCO868LR}`, function () {
-      assert.equal(51.6019287109375, converter.CO868LR(payloadCO868LR).RHValue);
+      expect(converter.CO868LR(payloadCO868LR).RHValue).to.eq(51.6019287109375);
     });
 
     it(`should return 693 CO2Value with payload ${payloadCO868LR}`, function () {
-      assert.equal(693, converter.CO868LR(payloadCO868LR).CO2Value);
+      expect(converter.CO868LR(payloadCO868LR).CO2Value).to.eq(693);
     });
   });
 
@@ -29,23 +29,23 @@ describe('Ascoel', function () {
   describe('IR868LR', function () {
     var payloadIR868LR = 'e40008ac'
     it(`should return 100 BatteryLevel with payload ${payloadIR868LR}`, function () {
-      assert.equal(100, converter.IR868LR(payloadIR868LR).BatteryLevel);
+      expect(converter.IR868LR(payloadIR868LR).BatteryLevel).to.equal(100);
     });
 
     it(`should return 1 BatteryType with payload ${payloadIR868LR}`, function () {
-      assert.equal(1, converter.IR868LR(payloadIR868LR).BatteryType);
+      expect(converter.IR868LR(payloadIR868LR).BatteryType).to.eq(1);
     });
 
     it(`should return 0 BatteryAlarm with payload ${payloadIR868LR}`, function () {
-      assert.equal(0, converter.IR868LR(payloadIR868LR).BatteryAlarm);
+      expect(converter.IR868LR(payloadIR868LR).BatteryAlarm).to.eq(0);
     });
 
     it(`should return 0 IntrusionAlarm with payload ${payloadIR868LR}`, function () {
-      assert.equal(0, converter.IR868LR(payloadIR868LR).IntrusionAlarm);
+      expect(converter.IR868LR(payloadIR868LR).IntrusionAlarm).to.eq(0);
     });
 
     it(`should return 8 OpCnt with payload ${payloadIR868LR}`, function () {
-      assert.equal(8, converter.IR868LR(payloadIR868LR).OpCnt);
+      expect(converter.IR868LR(payloadIR868LR).OpCnt).to.eq(8);
     });
   });
 });
