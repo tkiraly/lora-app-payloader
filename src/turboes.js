4bddf48944cd41f8872fd16ed9745ae106b6592d
Tamás Király <kiraly.tamas@outlook.com>
2017-02-07 16:03:25 +0100

    refactor(Turboes-TBS200): reimplemented parsing with binary-parser

--- src/turboes.js
+++ src/turboes.js
@@ -1,3 +1,5 @@
+var Parser = require('binary-parser').Parser;
+
 module.exports = {
     TBS200: function (payload) {
         if (payload) {
@@ -11,63 +13,64 @@ module.exports = {
                 default:
                     return null;
             }
-        }
-        else return null;
+        } else return null;
     }
 }
 
 function parseuplinkstatusframe(payload) {
-    var obj = {};
-    obj.FrameType = payload.substr(0, 2);
-    obj.FrameCount = payload.substr(2, 1);
-    obj.Status = payload.substr(3, 1);
-
-    let byte2 = parseInt(payload.substr(4, 2), 16);
-    obj.ParkFlag = (byte2 & 0x80) >> 7;
-    obj.BatteryLevel = byte2 & 0x7f;
-    obj.Reserved = payload.substr(6, 2);
-    obj.FrameEnd = payload.substr(8, 2);
-    return obj;
+    var decoded = new Parser()
+        .endianess('big')
+        .skip(1)
+        .bit4('FrameCount')
+        .bit4('Status')
+        .bit1('ParkFlag')
+        .bit7('BatteryLevel')
+        .uint8('Reserved')
+    var buf = new Buffer(payload, 'hex');
+    return Object.assign(decoded.parse(buf), {
+        FrameType: "ab",
+        FrameEnd: payload.substr(8, 2)
+    });
 }
 
 function parseuplinkparametersframe(payload) {
-    var obj = {};
-    obj.FrameType = payload.substr(0, 2);
-
-    let byte1 = parseInt(payload.substr(2, 2), 16);
-    obj.HWVersion = byte1 & 0xe0;
-    obj.SWVersion = byte1 & 0x1f;
-
-    let byte2 = parseInt(payload.substr(4, 2), 16);
-    obj.WM = byte2 & 0x40;
-    obj.HeartbeatInterval = byte2 & 0x1c;
-    obj.Sensitivity = byte2 & 0x07;
-
-    obj.Reserved = payload.substr(6, 2);
-    obj.FrameEnd = payload.substr(8, 2);
-    return obj;
+    var decoded = new Parser()
+        .endianess('big')
+        .skip(1)
+        .bit3('HWVersion')
+        .bit5('SWVersion')
+        .bit1('Unused')
+        .bit1('WM')
+        .bit3('HeartbeatInterval')
+        .bit3('Sensitivity')
+        .uint8('Reserved')
+    var buf = new Buffer(payload, 'hex');
+    return Object.assign(decoded.parse(buf), {
+        FrameType: "ac",
+        FrameEnd: payload.substr(8, 2)
+    });
 }
 
 function parsedownlinkcommandframe(payload) {
-    var obj = {};
-    obj.FrameType = payload.substr(0, 2);
-
-    let byte1 = parseInt(payload.substr(2, 2), 16);
-    obj.Reset = byte1 & 0x80;
-    obj.ParaInq = byte1 & 0x20;
-    obj.Upgrade = byte1 & 0x10;
-    obj.SetHBI = byte1 & 0x08;
-    obj.SetSens = byte1 & 0x04;
-    obj.Calib = byte1 & 0x02;
-    obj.SetWM = byte1 & 0x01;
-
-    let byte2 = parseInt(payload.substr(4, 2), 16);
-    obj.CalibMode = byte2 & 0x80;
-    obj.WM = byte2 & 0x40;
-    obj.Sensitivity = byte2 & 0x38;
-    obj.HeartbeatInterval = byte2 & 0x07;
-
-    obj.Reserved = payload.substr(6, 2);
-    obj.FrameEnd = payload.substr(8, 2);
-    return obj;
+    var decoded = new Parser()
+        .endianess('big')
+        .skip(1)
+        .bit1('Reset')
+        .bit1('Unused')
+        .bit1('ParaInq')
+        .bit1('Upgrade')
+        .bit1('SetHBI')
+        .bit1('SetSens')
+        .bit1('Calib')
+        .bit1('SetWM')
+        .bit1('CalibMode')
+        .bit1('WM')
+        .bit3('Sensitivity')
+        .bit3('HeartbeatInterval')
+        .uint8('Reserved')
+    var buf = new Buffer(payload, 'hex');
+    return Object.assign(decoded.parse(buf), {
+        FrameType: "ad",
+        FrameEnd: payload.substr(8, 2)
+    });
 }
--- test/turboes.js
+++ test/turboes.js
@@ -5,14 +5,56 @@ describe('Turboes', function () {
   describe('TBS200', function () {
     describe('uplink status', function () {
       var payloadTBS200 = 'ab005a80ae'
-      it(`should return 0 Status with payload ${payloadTBS200}`, function () {
+      it(`should return ab FrameType with payload ${payloadTBS200}`, function () {
         expect(converter.TBS200(payloadTBS200).FrameType).to.eq("ab");
+      });
+      it(`should return 0 FrameCount with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).FrameCount).to.eq(0);
+      });
+      it(`should return 0 Status with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Status).to.eq(0);
+      });
+      it(`should return 0 ParkFlag with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).ParkFlag).to.eq(0);
+      });
+      it(`should return 90 BatteryLevel with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).BatteryLevel).to.eq(90);
+      });
+      it(`should return 128 Reserved with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Reserved).to.eq(128);
+      });
+      it(`should return ae FrameEnd with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).FrameEnd).to.eq("ae");
       });
     });
     describe('uplink parameters', function () {
       var payloadTBS200 = 'ac005a80ae'
-      it(`should return 0 Status with payload ${payloadTBS200}`, function () {
+      it(`should return 0 FrameType with payload ${payloadTBS200}`, function () {
         expect(converter.TBS200(payloadTBS200).FrameType).to.eq("ac");
+      });
+      it(`should return 0 HWVersion with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).HWVersion).to.eq(0);
+      });
+      it(`should return 0 SWVersion with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).SWVersion).to.eq(0);
+      });
+      it(`should return 0 Unused with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Unused).to.eq(0);
+      });
+      it(`should return 1 WM with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).WM).to.eq(1);
+      });
+      it(`should return 3 HeartbeatInterval with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).HeartbeatInterval).to.eq(3);
+      });
+      it(`should return 2 Sensitivity with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Sensitivity).to.eq(2);
+      });
+      it(`should return 128 Reserved with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Reserved).to.eq(128);
+      });
+      it(`should return ae FrameEnd with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).FrameEnd).to.eq("ae");
       });
     });
     describe('downlink command', function () {
@@ -20,8 +62,48 @@ describe('Turboes', function () {
       it(`should return 0 Status with payload ${payloadTBS200}`, function () {
         expect(converter.TBS200(payloadTBS200).FrameType).to.eq("ad");
       });
+      it(`should return 0 Reset with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Reset).to.eq(0);
+      });
+      it(`should return 0 Unused with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Unused).to.eq(0);
+      });
+      it(`should return 0 ParaInqStatus with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).ParaInq).to.eq(0);
+      });
+      it(`should return 0 Upgrade with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Upgrade).to.eq(0);
+      });
+      it(`should return 0 SetHBI with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).SetHBI).to.eq(0);
+      });
+      it(`should return 0 SetSens with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).SetSens).to.eq(0);
+      });
+      it(`should return 0 Calib with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Calib).to.eq(0);
+      });
+      it(`should return 0 SetWM with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).SetWM).to.eq(0);
+      });
+      it(`should return 0 CalibMode with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).CalibMode).to.eq(0);
+      });
+      it(`should return 1 WM with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).WM).to.eq(1);
+      });
+      it(`should return 3 Sensitivity with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Sensitivity).to.eq(3);
+      });
+      it(`should return 2 HeartbeatInterval with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).HeartbeatInterval).to.eq(2);
+      });
+      it(`should return 128 Reserved with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).Reserved).to.eq(128);
+      });
+      it(`should return ae FrameEnd with payload ${payloadTBS200}`, function () {
+        expect(converter.TBS200(payloadTBS200).FrameEnd).to.eq("ae");
+      });
     });
   });
 });
-
-
