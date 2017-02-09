7cecf554df5871bcddb010f7c1459b4b54a0181f
Tamás Király <kiraly.tamas@outlook.com>
2017-02-09 01:47:36 +0100

    feat(risinghf): added rising hf RHF1S001

---
+++ src/risinghf.js
@@ -0,0 +1,31 @@
+var Parser = require('binary-parser').Parser;
+
+module.exports = {
+    RHF1S001: function (payload) {
+        if (payload) {
+            return parse(payload);
+        } else return null;
+    }
+}
+
+function parse(payload) {
+    var decoded = new Parser()
+        .endianess('little')
+        .uint8('Header')
+        .uint16('Temperature')
+        .uint8('Humidity')
+        .uint16('Period')
+        .uint8('RSSI')
+        .int8('SNR')
+        .uint8('Battery')
+    var buf = new Buffer(payload, 'hex');
+    var obj = decoded.parse(buf)
+    obj.Temperature = 175.72 * obj.Temperature / Math.pow(2, 16) - 46.85
+    obj.Humidity = 125 * obj.Humidity / Math.pow(2, 8) - 6
+    obj.Period = obj.Period * 2
+    obj.RSSI = obj.RSSI === 0xff ? null : -180 + obj.RSSI
+    //var SNR = obj.SNR && 0x80 ? -(~obj.SNR + 1) : obj.SNR
+    obj.SNR =  obj.SNR / 4
+    obj.Battery = (obj.Battery + 150) * 0.01
+    return obj
+}
